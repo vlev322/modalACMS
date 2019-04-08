5977965534434c95b6d979b809c6e02793f54d85
vlev322 <vlev322@gmail.com>
2019-04-08 13:19:34 +0300

    Feature (working modal window)

--- src/ts/view/components/modal/index.tsx
+++ src/ts/view/components/modal/index.tsx
@@ -1,17 +1,40 @@
-import React, { Component, Children } from 'react';
+import React, { Component } from 'react';
 
-export class Modal extends Component<{}, {}> {
-	public render(): JSX.Element {
+export class Modal extends Component<{}, { isOpen: boolean }> {
+	constructor(props: any) {
+		super(props);
+		this.state = { 
+			isOpen: true 
+		};
+		this.close = this.close.bind(this);
+		this.isOpen = this.isOpen.bind(this);
+	}
+	public render(): JSX.Element | null {
+		return (
+			this.isOpen()
+		)
+	};
+
+	close() {
+		this.setState({ isOpen: false })
+	}
+	isOpen() {
+		return (
+			this.state.isOpen ? this.modal() : null
+		)
+	}
+
+	modal() {
 		return (
 			<div className='modal-wrapper'>
 				<div className='modal'>
 					<div className='modal-header'>
-						<span>Add new path</span>
-						<button>&#10006;</button>
+						<span>Header modal window</span>
+						<button onClick={this.close}>&#10006;</button>
 					</div>
 					{this.props.children}
 				</div>
 			</div>
 		)
-	};
+	}
 }
--- src/ts/view/index.tsx
+++ src/ts/view/index.tsx
@@ -6,7 +6,10 @@ export class IndexView extends Component<{}, {}> {
 	public render(): JSX.Element {
 		return (
 			<Fragment>
-				<Modal><h1>Our Children</h1></Modal>
+				<button>Open modal</button>
+				<Modal><h1>Hello this is window #1</h1></Modal>
+				<Modal><h1>Hello this is window #2</h1></Modal>
+				<Modal><h1>Hello this is window #3</h1></Modal>
 			</Fragment>
 		);
 	}
